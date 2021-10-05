9f0d1710d7c06b1d50f34a3e7b8ddcb1bfc1abbf
uilmi <uilmi@na.edu>
2021-10-05 23:05:29 +0700

    init auth node modules

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const session = require('express-session');
+const flash = require('express-flash');
+
+//const { PORT = 8000 } = process.env
+const PORT = process.env.PORT || 8000;
+
+// 1. Setting request body parser
+// (Penting!, body parser harus paling atas)
+app.use(express.urlencoded({ extended: false }));
+
+// 2. Setting session handler
+app.use(session({
+    secret: 'Buat ini jadi rahasia',
+    resave: false,
+    saveUninitialized: false
+}));
+
+// 3. setting passport
+// (sebelum router dan view engine)
+const passport = require('passport');
+app.use(passport.initialize());
+app.use(passport.session());
+
+// 4. setting flash
+app.use(flash());
+
+// 5. view engine
+app.set('view engine', 'ejs');
+
+// 6. setting router
+const router = require('./router');
+app.use(router);
+
+
+app.listen(PORT, () => {
+    console.log(`Server nyala di http://localhost:${PORT}`);
+})
